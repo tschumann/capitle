113923ac3e299fc42d7ea7a5cb2cf6e696ef2a3a
tschumann <schumann.tom@gmail.com>
2022-05-07 21:48:20 +1000

    Fixed deployment not deploying all files.

--- install.sh
+++ install.sh
@@ -12,6 +12,7 @@ sudo cp capitle.conf /etc/nginx/sites-enabled
 sudo cp index.html /var/www/sites/capitle
 sudo cp capitle.js /var/www/sites/capitle
 sudo cp styles.css /var/www/sites/capitle
+sudo cp country-capitals.json /var/www/sites/capitle
 sudo cp sitemap.xml /var/www/sites/capitle
 
 sudo service nginx restart
