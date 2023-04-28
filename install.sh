dc803e3898ae25333175be943fc9143a3e0da65b
tschumann <schumann.tom@gmail.com>
2023-04-29 08:30:34 +1000

    Fixed favicon not getting installed.

--- install.sh
+++ install.sh
@@ -16,6 +16,7 @@ sudo cp index.html /var/www/sites/capitle
 sudo cp capitle.js /var/www/sites/capitle
 sudo cp styles.css /var/www/sites/capitle
 sudo cp country-capitals.json /var/www/sites/capitle
+sudo cp favicon.png /var/www/sites/capitle
 sudo cp sitemap.xml /var/www/sites/capitle
 
 sudo service nginx restart
